ad8b0ccc71161ac0545acf27fe88d296f8e4b1d6
LabheshBhurewal <bhurewallabhesh05@gmail.com>
2024-02-10 15:48:23 +0530

    Created New Chat App Project

---
+++ ChatApp/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Chat from './Components/Chat/Chat'
+
+const App = () => {
+  return (
+   <Chat/>  )
+}
+
+export default App
---
+++ ChatApp/src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Chat from '../../Components/Chat/Chat'
+
+const Home = () => {
+  return (
+    <Chat/>
+  )
+}
+
+export default Home
