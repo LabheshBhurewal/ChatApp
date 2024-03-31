b3b10c3a84a79c0a0340e55a671cc1ac8f1ca955
LabheshBhurewal <bhurewallabhesh05@gmail.com>
2024-03-31 18:03:25 +0530

    react firebase authentication

--- ChatApp/src/App.jsx
+++ ChatApp/src/App.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import Home from './Pages/Home/Home'
+import React from "react";
+import Home from "./Pages/Home/Home";
 import "./style.scss";
-import Register from './Pages/Register/Register';
-import Login from './Pages/Login/Login';
+import Register from "./Pages/Register/Register";
+import Login from "./Pages/Login/Login";
+// import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthContext } from "./context/Authcontext";
+import { useContext } from "react";
 const App = () => {
-  return (
-     <Register/>
-    )
-}
 
-export default App
+ const {currentUser}= useContext(AuthContext)
+console.log(currentUser)
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+           <Route index element={<Home/>}/>
+           <Route path="login" element={<Login/>}/>
+           <Route path="register" element={<Register/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
--- ChatApp/src/Pages/Register/Register.jsx
+++ ChatApp/src/Pages/Register/Register.jsx
@@ -4,10 +4,12 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, storage, db } from "../../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   // const fileInputRef = useRef(null); // Create a ref for the file input
   const [err, setError] = useState(false);
+  const navigate =useNavigate()
   const handleSubmit = async (e) => {
     e.preventDefault();
     const displayName = e.target[0].value;
@@ -40,6 +42,8 @@ const Register = () => {
               email,
               photoURL: downloadURL,
             });
+            await setDoc(doc(db,"userschat",res.user.uid),{})
+            navigate("/")
           });
         }
       );
